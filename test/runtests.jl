8bfe3a348b8df31d424b60a93901f2b2fa98a203
Nathan Daly <nhdaly@gmail.com>
2020-04-28 20:58:16 -0400

    Test the examples/ packages in Mutts tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -151,4 +151,20 @@ end
     end
 end
 
+# -----------------------------------------------------------
+# --- Example packages
+# -----------------------------------------------------------
+
+import Pkg
+
+# Run the tests for the packages in examples/...
+#@testset "examples" begin
+  Pkg.activate("../examples/MuttsVersionedTrees")
+  Pkg.test(coverage=true)
+#end
+
+# Done runing example packages
+Pkg.activate(".")
+
+# -----------------------------------------------------------
 end
