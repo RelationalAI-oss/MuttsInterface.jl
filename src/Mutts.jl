f6688180f75ea87f4613afc8ff2f2ffde29b89ba
Joe Miller <arch1190@gmail.com>
2021-02-01 16:42:26 -0600

    Update src/Mutts.jl
    Co-authored-by: Nathan Daly <44379820+rai-nhdaly@users.noreply.github.com>

--- src/Mutts.jl
+++ src/Mutts.jl
@@ -75,7 +75,7 @@ branchactions(obj) = nothing
 """
     mark_immutable!(obj)
 
-Freeeze `obj` from further mutations, making it eligible to pass to
+Freeze `obj` from further mutations, making it eligible to pass to
 other Tasks, branch! from it, or otherwise share it.
 """
 function mark_immutable! end
